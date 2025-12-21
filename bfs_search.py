df81ba507d92b4e00c36e5dd82aa07429fa4f81a
fajar_kkp <fajarputra4231@gmail.com>
2025-12-21 09:06:28 +0000

    bfs

---
+++ bfs_search.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def bfs_search(root):
+    if not root:
+        return[]
+    
+    queue = deque([root])
+    result = []
+
+    while queue:
+        node = queue.popleft()
+        result.append(node.val)
+
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    
+    return result
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+
+print(bfs_search(root))
